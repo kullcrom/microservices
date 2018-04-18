c91315ee2d708dac8451fbb53f7d07a937f728a9
Jeff Steed <jstff@arity.com>
2018-04-18 16:03:55 -0500

    create microservices dir and npm init

---
+++ zmq-filer-req-loop.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const zmq = require('zeromq');
+const filename = process.argv[2];
+
+// Create request endpoint.
+const requester = zmq.socket('req');
+
+// Handle replies from the responder.
+requester.on('message', data => {
+    const response = JSON.parse(data);
+    console.log('Received response:', response);
+});
+
+requester.connect('tcp://localhost:60401');
+
+for (let i = 1; i <= 5; i++) {
+    // Send a request for content.
+    console.log(`Sending request ${i} for ${filename}`);
+    requester.send(JSON.stringify({path: filename}));
+}
